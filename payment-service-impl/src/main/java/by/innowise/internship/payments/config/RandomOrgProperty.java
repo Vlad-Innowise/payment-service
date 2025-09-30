a3e072f26831131f383e150bcc3840e8d83cf3aa
Vlad Golubev <holybay1886@gmail.com>
2025-09-30 23:59:05 +0300

    Fixed: set MAX internal service random int value. Removed @Value

--- payment-service-impl/src/main/java/by/innowise/internship/payments/config/RandomOrgProperty.java
+++ payment-service-impl/src/main/java/by/innowise/internship/payments/config/RandomOrgProperty.java
@@ -1,23 +1,21 @@
 package by.innowise.internship.payments.config;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
-@ConfigurationProperties
+@ConfigurationProperties(prefix = "application.feign.clients.random-org")
 public class RandomOrgProperty {
 
+    private static final int EXTERNAL_SERVICE_MAX_INT_VALUE = 1_000_000_000;
     private static final String INTEGER_FORMAT_PLAIN = "plain";
     private static final String RANDOM_GEN_TYPE = "new";
-
+    
     private int num = 1;
 
-    @Value("${application.feign.clients.random-org.min}")
     private int min = 1;
 
-    @Value("${application.feign.clients.random-org.max}")
-    private int max = Integer.MAX_VALUE;
+    private int max = EXTERNAL_SERVICE_MAX_INT_VALUE;
 
     private int col = 1;
 

f69a93472c81e7d839ffd99b83d9667a6e548250
Vlad Golubev <holybay1886@gmail.com>
2025-10-01 19:26:58 +0300

    Added POST /payments/user/{userId} end-point.

---
+++ payment-service-impl/src/main/java/by/innowise/internship/payments/controller/PaymentController.java
@@ -0,0 +1,33 @@
+package by.innowise.internship.payments.controller;
+
+import by.innowise.internship.payments.model.dto.PaymentRequestDto;
+import by.innowise.internship.payments.model.dto.PaymentResponseDto;
+import by.innowise.internship.payments.service.PaymentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments/user/{userId}")
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<PaymentResponseDto> createPayment(@PathVariable Long userId,
+                                                            @RequestBody @Valid PaymentRequestDto newPayment) {
+        log.info("Requested to process a new payment for user: {}: {}", newPayment, userId);
+        PaymentResponseDto created = paymentService.create(newPayment, userId);
+        log.info("Payment request was processed. Sending response to the client: {}", created);
+        return ResponseEntity.ok(created);
+    }
+
+}
